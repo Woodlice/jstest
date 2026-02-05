bb8033c0184dcd090394c8cb645853077f753961
Woodlice <rcanderson.84@googlemail.com>
2026-02-05 16:17:39 +0000

    Update tessaract-tablesorter-init.js

--- tessaract-tablesorter-init.js
+++ tessaract-tablesorter-init.js
@@ -1,4 +1,4 @@
 document.body.insertAdjacentHTML(
   "afterbegin",
-  "<div style='padding:8px;background:#ffd;border:2px solid #cc0;z-index:99999'>INIT LOADED (jsDelivr)</div>"
+  "<div style='padding:8px;background:#ffd;border:2px solid #cc0'>INIT LOADED (jsDelivr)</div>"
 );
