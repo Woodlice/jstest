0ef6d94e4d01910072f6409300333420d11e1968
Woodlice <rcanderson.84@googlemail.com>
2026-02-05 16:09:23 +0000

    Update tessaract-tablesorter-init.js

--- tessaract-tablesorter-init.js
+++ tessaract-tablesorter-init.js
@@ -1,4 +1,14 @@
-document.body.insertAdjacentHTML(
-  "afterbegin",
-  "<div style='padding:8px;background:#ffd;border:1px solid #cc0'>INIT LOADED</div>"
-);
+(async () => {
+  for (let i = 0; i < 200; i++) {
+    const ok = window.jQuery && jQuery.fn && jQuery.fn.tablesorter && jQuery("#matterActivities").length;
+    if (ok) break;
+    await new Promise(r => setTimeout(r, 100));
+  }
+
+  if (window.jQuery && jQuery.fn && jQuery.fn.tablesorter) {
+    jQuery("#matterActivities").tablesorter({
+      theme: "bootstrap",
+      headers: { 6: { sorter: false } }
+    });
+  }
+})();
